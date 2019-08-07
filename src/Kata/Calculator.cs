203fc107f623b0fd4c26057c344530967068a409
Yamil Gonzales <yamil@live.com>
2019-08-07 14:50:48 -0600

    pong

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -7,7 +7,15 @@ namespace Kata
             if(string.IsNullOrEmpty(s))
                 return 0;
 
-            return 1;
+            var strings = s.Split(',');
+            var r = int.Parse(strings[0]);
+            if (strings.Length>1)
+            {
+                var y = int.Parse(strings[1]);
+                return r + y;
+            }
+
+            return r;
         }
     }
 }
