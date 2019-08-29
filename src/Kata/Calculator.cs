7cebc92a091d586b1bac8abd8ee4e85213e1693e
Yamil Gonzales <yamil@live.com>
2019-08-29 09:29:40 -0600

    two numbers passing; unknown amount of numbers failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -5,7 +5,15 @@ namespace Kata
         public int Add(string s = "")
         {
             if(string.IsNullOrEmpty(s)) return 0;
+
+            if (s.Length > 1)
+            {
+                var strings = s.Split(",");
+                return int.Parse(strings[0]) + int.Parse(strings[1]);
+            }
+
             return 3;
+
         }
     }
 }
